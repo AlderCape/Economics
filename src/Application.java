1ff318a2c0c23aa374dedc1730da508272f0c398
Johan Aludden <johan@aldercape.com>
2012-01-11 01:49:20 +0100

    [Fixes #23142195] agin to test the integration

--- src/Application.java
+++ src/Application.java
@@ -6,6 +6,7 @@ public class Application {
 
 	public static void main(String[] args) {
 		System.setProperty("apple.laf.useScreenMenuBar", "true");
+
 		LedgerFrame frame = new LedgerFrame(new ApplicationModel(new Ledger()));
 		frame.pack();
 		frame.setVisible(true);
