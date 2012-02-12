6c4b37d2993e9948fc04695549d28d4dab988b0d
Johan Aludden <johan@aldercape.com>
2012-02-13 00:52:32 +0100

    Started on members in project

--- src/com/aldercape/internal/economics/model/Project.java
+++ src/com/aldercape/internal/economics/model/Project.java
@@ -3,13 +3,28 @@ package com.aldercape.internal.economics.model;
 public class Project {
 
 	private String name;
+	private Client client;
+	private int memberCount;
 
-	public Project(String name) {
+	public Project(String name, Client client) {
 		this.name = name;
+		this.client = client;
 	}
 
 	public String name() {
 		return name;
+	}
+
+	public Client client() {
+		return client;
+	}
+
+	public void addMember(Collaborator member, Rate rate) {
+		memberCount++;
+	}
+
+	public int memberCount() {
+		return memberCount;
 	}
 
 }
--- test/com/aldercape/internal/economics/model/ProjectTest.java
+++ test/com/aldercape/internal/economics/model/ProjectTest.java
@@ -1,15 +1,21 @@
 package com.aldercape.internal.economics.model;
 
+import static com.aldercape.internal.economics.model.CustomModelAsserts.*;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
+
+import com.aldercape.internal.economics.ui.__TestObjectMother;
 
 public class ProjectTest {
 
 	@Test
 	public void test() {
-		Project project = new Project("Project name");
+		__TestObjectMother objectMother = new __TestObjectMother();
+		Project project = new Project("Project name", objectMother.otherCompany());
+		project.addMember(objectMother.me(), Rate.daily(new Euro(200)));
 		assertEquals("Project name", project.name());
+		assertClientEquals(objectMother.otherCompany(), project.client());
+		assertEquals(1, project.memberCount());
 	}
-
 }
